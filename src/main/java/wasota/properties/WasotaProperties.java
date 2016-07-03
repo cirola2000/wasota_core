82f7a7694037132537251a75aab3ac6a6dbb56eb
cirola2000 <cirobaroneto@gmail.com>
2016-07-03 22:35:06 +0200

    mex vucabularies address changed

--- src/main/java/wasota/properties/WasotaProperties.java
+++ src/main/java/wasota/properties/WasotaProperties.java
@@ -41,9 +41,12 @@ public class WasotaProperties {
 
 	final public static String BASE_PATH = "./graphs/";
 	final public static String INDEX_PATH = "./index/index";
-	final public static String MEX_CORE_DOWNLOAD = "http://localhost:3010/files/mexcore.ttl";
-	final public static String MEX_PERF_DOWNLOAD = "http://localhost:3010/files/mexperf.ttl";
-	final public static String MEX_ALGO_DOWNLOAD = "http://localhost:3010/files/mexalg.ttl";
+//	final public static String MEX_CORE_DOWNLOAD = "http://localhost:3010/files/mexcore.ttl";
+	final public static String MEX_CORE_DOWNLOAD = "http://mex.aksw.org/mex-core";
+//	final public static String MEX_PERF_DOWNLOAD = "http://localhost:3010/files/mexperf.ttl";
+	final public static String MEX_PERF_DOWNLOAD = "http://mex.aksw.org/mex-perf";
+//	final public static String MEX_ALGO_DOWNLOAD = "http://localhost:3010/files/mexalg.ttl";
+	final public static String MEX_ALGO_DOWNLOAD = "http://mex.aksw.org/mex-algo";
 
 	// mongodb properties
 	public static String MONGODB_HOST;
